2f08447ef3dd18f453066dfe6793364582c74342
Bakytai Egemberdieva <bakijw@gmail.com>
2022-04-22 16:11:04 +0600

    #19 реализовала отображение подкатегорий при наведении на категорий в выпадающем меню

--- frontend/src/app/ui/toolbar/toolbar.component.ts
+++ frontend/src/app/ui/toolbar/toolbar.component.ts
@@ -6,6 +6,8 @@ import { AppState } from '../../store/types';
 import { logoutUserRequest } from '../../store/users/users.actions';
 import { Category } from '../../models/category.model';
 import { deleteCategoryRequest, fetchCategoriesRequest } from '../../store/categories/categories.actions';
+import { fetchSubcategoriesByCategoryRequest } from '../../store/subcategories/subcategories.actions';
+import { Subcategory } from '../../models/subcategory.model';
 
 @Component({
   selector: 'app-toolbar',
@@ -17,6 +19,9 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   categories: Observable<Category[]>;
   categoriesArray!: Category[];
   categorySubscription!: Subscription;
+  subCategories: Observable<Subcategory[]>;
+  subCategoriesArray!: Subcategory[];
+  subCategorySubscription!: Subscription;
   fetchLoadingCategory: Observable<boolean>;
   fetchLoadingError: Observable<null | string>
   search = '';
@@ -25,16 +30,23 @@ export class ToolbarComponent implements OnInit, OnDestroy {
     this.user = store.select(state => state.users.user);
     this.categories = store.select(state => state.categories.categories);
     this.fetchLoadingCategory = store.select(state => state.categories.fetchLoading);
-    this.fetchLoadingError = store.select(state => state.categories.fetchLoadingError)
+    this.fetchLoadingError = store.select(state => state.categories.fetchLoadingError);
+    this.subCategories = store.select(state => state.subcategories.subcategories);
   }
 
   ngOnInit() {
     this.store.dispatch(fetchCategoriesRequest());
+
     this.categorySubscription = this.categories.subscribe(categories => {
       if (categories) {
         this.categoriesArray = categories;
       }
-    })
+    });
+    this.subCategorySubscription = this.subCategories.subscribe(subCategories => {
+      if (subCategories) {
+        this.subCategoriesArray = subCategories;
+      }
+    });
   }
 
   logout(){
@@ -45,7 +57,12 @@ export class ToolbarComponent implements OnInit, OnDestroy {
     this.store.dispatch(deleteCategoryRequest({id}))
   }
 
+  fetchSubCategory(id: string) {
+    this.store.dispatch(fetchSubcategoriesByCategoryRequest({id}));
+  }
+
   ngOnDestroy(): void {
     this.categorySubscription.unsubscribe();
+    this.subCategorySubscription.unsubscribe();
   }
 }
