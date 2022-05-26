184943b8ade4e6985a9b855fee113005d4a19933
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-05-26 14:52:10 +0600

    #75 Авто поставка приложения. 1. Изменения в файле env.production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://134.122.65.122/:8000',
+  fbAppId: '407104017926294',
+  googleAppId: '324167406660-o6ge0kgc0d1vknbjsb1366ao5np0vdns.apps.googleusercontent.com'
 };
