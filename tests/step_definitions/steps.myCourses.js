a3361db7c11cbcdc5540ecf8151db166be6e4a56
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-05-28 18:23:26 +0600

    #67 Вывести процент прохождения курса 1) Отрефакторены тесты @learningCourses, @favoriteCourses 2) Добавлен тест на прохождение курса @takeTheCourse

--- tests/step_definitions/steps.myCourses.js
+++ tests/step_definitions/steps.myCourses.js
@@ -1,11 +1,40 @@
-const { I } = inject();
+const {I} = inject();
 
-Given('я должен увидеть курсы, которые прохожу', () => {
-    I.seeElementInDOM('.learning');
-    I.wait(1);
+Given('я добавил курс на страницу {string}', (courses) => {
+  I.click('Java Script');
+  I.wait(1);
+
+  I.seeElementInDOM('.course-title');
+  I.wait(1);
+
+  const learnBtn = courses.split(',')[1] ? 'Хочу пройти' : 'Поступить на курс';
+
+  I.click(learnBtn);
+  I.wait(1);
+
+  const message = courses.split(',')[1] ? 'Добавлен в список желаний' : 'Добавлен в мои курсы';
+
+  I.see(message);
+
+  const page = courses.split(',')[1] ? 'favorite' : 'learning';
+
+  I.amOnPage(`/myCourses/${page}`);
+
+  const card = courses.split(',')[1] ? '.favorite' : '.learning';
+
+  I.seeElementInDOM(card);
+  I.wait(1);
 });
 
-Given('я должен увидеть курсы, которые хочу пройти', () => {
-    I.seeElementInDOM('.favorite');
-    I.wait(1);
+Given('я прошел курс', () => {
+  I.click('Продолжить');
+  I.wait(1);
+
+  I.click('Следующий урок');
+  I.wait(1);
+
+  I.click('Мои курсы');
+  I.wait(1);
+
+  I.see('50%');
 });
