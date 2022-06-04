73ad9585a682534854ed64456629a27fa68c7d6f
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-06-04 21:25:10 +0600

    #81 Редактирование отображения сайдбара на странице "Мои курсы" и курсов в разделе "Хочу пройти".

--- frontend/src/app/pages/my-courses/my-courses.component.ts
+++ frontend/src/app/pages/my-courses/my-courses.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { User } from '../../models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/types';
+import { fetchUserRequest } from '../../store/users/users.actions';
 
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.css']
 })
-export class MyCoursesComponent implements OnInit {
+export class MyCoursesComponent implements OnInit, OnDestroy {
+  user: Observable<null | User>;
+  userSub!: Subscription;
+  userData!: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>) {
+    this.user = store.select(state => state.users.user);
   }
 
+  ngOnInit(): void {
+    this.store.dispatch(fetchUserRequest())
+    this.userSub = this.user.subscribe(userData => {
+      if (userData) {
+        this.userData = userData;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
